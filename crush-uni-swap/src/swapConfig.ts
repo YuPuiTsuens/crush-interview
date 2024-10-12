afb46caa4ba013a46bbd2243a3b91c478ac6599f
Peak Xiong <16359576+YuPuiTsuens@users.noreply.github.com>
2024-10-12 21:01:50 +0800

    first commit

--- crush-uni-swap/src/swapConfig.ts
+++ crush-uni-swap/src/swapConfig.ts
@@ -1,5 +1,5 @@
 export const swapConfig = {
-    tokenAAmount: 0.1, // Swap 0.01 SOL for USDC in this example
+    tokenAAmount: 0.1,
     tokenAAddress: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
     tokenBAddress: "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
 };
